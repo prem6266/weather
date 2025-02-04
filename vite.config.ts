f15066cd7858ad439a2485a77fd103d7397b9deb
Prem Kumar Joshi <premjoshi6266@gmail.com>
2025-02-04 17:42:59 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/deploy-github/",
+  base: "/weather-dashboard/",
   plugins: [react()],
   resolve: {
     alias: {
